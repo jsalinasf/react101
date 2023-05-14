efc6939fe9fee101de8b3a1e30332c698b78653d
Jorge <jorge.salinas.jsf@outlook.com>
2023-05-13 20:46:12 -0500

    Refs lesson finished - Refs used for simply reading

--- refs-read-inputs/src/components/Form.js
+++ refs-read-inputs/src/components/Form.js
@@ -1,17 +1,26 @@
+import { useRef } from "react";
+
 function Form() {
+  // using refs to read input values
+  // DO NOT use refs on controlled components
+  const nameRef = useRef();
+  const programRef = useRef();
+
   function formSubmitHandler(event) {
     event.preventDefault();
+    console.log("Entered Name:", nameRef.current.value);
+    console.log("Selected Program:", programRef.current.value);
   }
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameRef} />
       </div>
       <div className="form-control">
         <label htmlFor="program">Choose your program</label>
-        <select id="program">
+        <select id="program" ref={programRef}>
           <option value="basics">The Basics</option>
           <option value="advanced">Advanced Concepts</option>
           <option value="mastery">Mastery</option>
