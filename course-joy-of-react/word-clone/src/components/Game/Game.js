003bca6eced0a42bbec859b0c0c969b442ecf350
Jorge <jorge.salinas.jsf@outlook.com>
2023-07-17 15:20:20 -0500

    Game Logic added - letter position styled properly following instructor code

--- course-joy-of-react/word-clone/src/components/Game/Game.js
+++ course-joy-of-react/word-clone/src/components/Game/Game.js
@@ -9,7 +9,7 @@ import GuessResults from "../GuessResults/GuessResults";
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
-// console.info({ answer });
+console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
