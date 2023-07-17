94d3b0c31621712d710543de48443ea9daed699f
Jorge <jorge.salinas.jsf@outlook.com>
2023-07-17 14:32:03 -0500

    Game Logic added - letter position styled properly

--- course-joy-of-react/word-clone/src/components/Guess/Guess.js
+++ course-joy-of-react/word-clone/src/components/Guess/Guess.js
@@ -1,12 +1,27 @@
 import React from "react";
 
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ value }) {
+function Guess({ value, answer }) {
+  const validation = checkGuess(value, answer);
+
+  function chooseClass(index) {
+    const returnValue = validation[index].status;
+    switch (returnValue) {
+      case "correct":
+        return "cell correct";
+      case "misplaced":
+        return "cell misplaced";
+      default:
+        return "cell";
+    }
+  }
+
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
+        <span key={num} className={value ? chooseClass(num) : "cell"}>
           {value ? value[num] : undefined}
         </span>
       ))}
