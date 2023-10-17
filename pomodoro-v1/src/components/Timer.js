becc933d9ce06f87a7a972cc9244c577ac6b4b1d
Jorge <jorge.salinas.jsf@outlook.com>
2023-10-17 13:14:44 -0500

    Pomodoro App - Basic timer functionality implemented, seconds countdown working partially

--- pomodoro-v1/src/components/Timer.js
+++ pomodoro-v1/src/components/Timer.js
@@ -1,20 +1,47 @@
 import React from "react";
 
 export default function Timer() {
+  const [isTimerRunning, setIsTimerRunning] = React.useState(false);
+  const [minutes, setMinutes] = React.useState(10);
+  const [seconds, setSeconds] = React.useState(60);
+
+  React.useEffect(() => {
+    if (isTimerRunning) {
+      const timeoutId = setTimeout(() => {
+        setSeconds((currentSeconds) => currentSeconds - 1);
+      }, 1000);
+      return () => {
+        window.clearInterval(timeoutId);
+      };
+    }
+  }, [isTimerRunning, seconds]);
+
+  function handleClickStart() {
+    setIsTimerRunning(!isTimerRunning);
+  }
+
+  function handleClickStop() {
+    setIsTimerRunning(!isTimerRunning);
+  }
+
   return (
     <div className="timer">
       <header>
         <h1>Pomodoro App</h1>
       </header>
       <main>
-        <div className="timer-chracters">10</div>
-        <div className="timer-chracters">:</div>
-        <div className="timer-chracters">00</div>
+        <span className="timer-characters">{minutes}</span>
+        <span className="timer-characters">:</span>
+        <span className="timer-characters">{seconds}</span>
       </main>
       <footer>
-        <button>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+        <button onClick={handleClickStart} disabled={isTimerRunning}>
+          Start
+        </button>
+        <button onClick={handleClickStop} disabled={!isTimerRunning}>
+          Stop
+        </button>
+        <button disabled={isTimerRunning}>Reset</button>
       </footer>
     </div>
   );
