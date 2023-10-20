1157f8dcbda481f794b98b1034fd30b96f8fa5e0
Jorge <jorge.salinas.jsf@outlook.com>
2023-10-20 12:58:59 -0500

    JS intervals dont run exactly in one second. For this app to run exactly another method should be implemented to manage time

--- pomodoro-v1/src/components/Timer.js
+++ pomodoro-v1/src/components/Timer.js
@@ -1,51 +1,36 @@
 import React from "react";
 
-const MINUTES = "01";
-const SECONDS = "10";
+const INITIALMINUTES = 0;
+const INITIALSECONDS = 5;
 
 export default function Timer() {
   const [isTimerRunning, setIsTimerRunning] = React.useState(false);
-  const [minutes, setMinutes] = React.useState(MINUTES);
-  const [seconds, setSeconds] = React.useState(SECONDS);
-
-  // TODO: check timer with actual timer. It seems to lag behind.
+  const [minutes, setMinutes] = React.useState(INITIALMINUTES);
+  const [seconds, setSeconds] = React.useState(INITIALSECONDS);
 
   React.useEffect(() => {
-    function handleTick() {
-      if (minutes === "00" && seconds === "00") {
-        console.log("Time is Up!");
-        setIsTimerRunning(false);
-        return;
-      }
-
-      // one second has passed, the clock has "ticked"
-      let nextSeconds = Number(seconds) - 1;
-
-      // if seconds reach -1, seconds need to be reseted to 59 - a new minute
-      if (nextSeconds === -1) {
-        nextSeconds = 59;
-        // Minutes are updated since a whole minute has passed
-        let nextMinutes = Number(minutes) - 1;
-        // format minutes to always show 2-digit format
-        if (nextMinutes.toString().length === 1) {
-          nextMinutes = nextMinutes.toString().padStart(2, 0);
-        }
-        setMinutes(nextMinutes);
-      }
-      // format seconds to always show 2-digit format
-      if (nextSeconds.toString().length === 1) {
-        nextSeconds = nextSeconds.toString().padStart(2, 0);
-      }
-      setSeconds(nextSeconds);
+    function isTimeUp() {
+      // check if time is up
+      if (minutes === 0 && seconds === 0) setIsTimerRunning(false);
     }
-    // it manages a second tick
+
     if (isTimerRunning) {
-      const timeoutId = setTimeout(() => {
-        handleTick();
+      const intervalId = setInterval(() => {
+        setSeconds((currentSeconds) => {
+          if (currentSeconds === 0) {
+            // one minute has elapsed
+            // minutes need to be decreased
+            setMinutes((currentMinutes) => currentMinutes - 1);
+            // seconds need to be reset
+            return 59;
+          } else {
+            // decrease seconds
+            return currentSeconds - 1;
+          }
+        });
       }, 1000);
-      return () => {
-        window.clearInterval(timeoutId);
-      };
+      isTimeUp();
+      return () => clearInterval(intervalId);
     }
   }, [isTimerRunning, minutes, seconds]);
 
@@ -58,8 +43,8 @@ export default function Timer() {
   }
 
   function handleClickReset() {
-    setMinutes(MINUTES);
-    setSeconds(SECONDS);
+    setMinutes(INITIALMINUTES);
+    setSeconds(INITIALSECONDS);
   }
 
   return (
@@ -67,10 +52,10 @@ export default function Timer() {
       <header>
         <h1>Pomodoro App</h1>
       </header>
-      <main>
-        <span className="timer-characters">{minutes}</span>
-        <span className="timer-characters">:</span>
-        <span className="timer-characters">{seconds}</span>
+      <main className="timer-characters">
+        <span>{minutes < 10 ? "0" + minutes : minutes}</span>
+        <span>:</span>
+        <span>{seconds < 10 ? "0" + seconds : seconds}</span>
       </main>
       <footer>
         <button onClick={handleClickStart} disabled={isTimerRunning}>
